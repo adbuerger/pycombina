e803bbc60e815a64cb21d03c7e0d0f7798a4b7e0
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2017-05-13 14:22:22 +0200

    profiling notebook

--- bnb_jung/setup.py
+++ bnb_jung/setup.py
@@ -2,13 +2,24 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+# from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
-ext_modules=[ Extension("bnb_jung",
+from Cython.Compiler.Options import get_directive_defaults
+
+directive_defaults = get_directive_defaults()
+
+directive_defaults['linetrace'] = True
+directive_defaults['binding'] = True
+directive_defaults['profile'] = True
+
+extensions=[ Extension("bnb_jung",
               ["bnb_jung.pyx"],
               libraries=["m"],
-              extra_compile_args = ["-ffast-math"])]
+              extra_compile_args = ["-ffast-math"], 
+              define_macros=[('CYTHON_TRACE', '1')])]
 
 setup(name="bnb_jung", \
-    cmdclass = {"build_ext": build_ext}, \
-    ext_modules=ext_modules,)
+    # cmdclass = {"build_ext": build_ext}, \
+    ext_modules=cythonize(extensions, \
+        compiler_directives=directive_defaults))
