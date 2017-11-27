32461cf0de2ca5b772c925decbf2e318f8aedb25
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2017-11-27 16:05:27 +0100

    remove debug flag

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class CMakeBuild(build_ext):
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + "Debug"]
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
