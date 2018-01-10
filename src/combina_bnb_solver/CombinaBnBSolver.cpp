da40c51d30aa67c77708a1923b271091dc40e79d
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2018-01-10 18:18:45 +0100

    adding copyright info and licence file

--- src/combina_bnb_solver/CombinaBnBSolver.cpp
+++ src/combina_bnb_solver/CombinaBnBSolver.cpp
@@ -2,8 +2,27 @@
  *
  * CombinaBnBSolver.cpp
  *
+ * -*- coding: utf-8 -*-
+ *
+ * This file is part of pycombina.
+ *
+ * Copyright 2017-2018 Adrian Bürger, Clemens Zeile, Sebastian Sager, Moritz Diehl
+ *
+ * pycombina is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pycombina is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pycombina. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
+ 
 #include <iostream>
 
 #ifndef ALGORITHM_H
