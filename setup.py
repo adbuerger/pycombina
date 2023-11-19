e37539a6c1f5ead6575f09da3eac61332fb963ea
Mirko Hahn <4210075+mirhahn@users.noreply.github.com>
2023-11-19 19:19:58 +0100

    Fixed extension module.
    Changes:
    - Removed unnecessary build dependency on 'GitPython'.
    - Added glob for '.cpp' files in 'src/combina_bnb_solver/'.

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ import glob
 from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 
-# Set up submodule.
+# Set up extension module.
 setup(
     ext_modules=[
         Pybind11Extension(
             name='pycombina._combina_bnb_solver',
-            sources=[*glob.glob('src/combina_bnb_solver/**/*.cpp')]
+            sources=[
+                *glob.glob('src/combina_bnb_solver/*.cpp'),
+                *glob.glob('src/combina_bnb_solver/**/*.cpp'),
+            ]
         ),
     ]
 )
